3cbd5d2cad16a74d29088a5b8effe193148bac5a
Demian Abiusi <demian@abiusi.com.ar>
2022-01-25 21:49:32 -0300

    Add notfound message

--- notfound.php
+++ notfound.php
@@ -1,6 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php require 'htmlhead.php'; ?>
+</head>
+<body>
+    
+    <?php require 'header.php'; ?>
+   
+    <main>
+        <div class="container text-center">
+                <h3 class="py-3">User not found. Please try again with a valid Ugsername</h3>
 
-print_r($_POST);
+                <form method="get" action="searchuser.php">
+                <div class="row">
+                    <div class="col-md-10">
+                        <input class="form-control" name="searchuser" placeholder="Please Enter a Valid Torre Username" >
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary">Display</button>
+                    </div>
+                </div>
+                </form>
+
+        </div>
+    </main>
 
 
-?>
+    <script>
+    </script>
+</body>
+</html>
